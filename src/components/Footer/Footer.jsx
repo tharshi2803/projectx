77ea6d2c99101fc5bfa2ef00399295d0609e35c3
tharshi2803 <67555968+tharshi2803@users.noreply.github.com>
2022-01-31 23:13:16 +0530

    features changed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,50 +5,43 @@ import '../Footer/footer.css';
 import { faYoutube, faFacebook, faTwitter, faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 
-const Footer = () => {
+function Footer() {
   return (
 
     <div class="d-flex flex-column">
 
-      <footer class="body1">
+      <div class="footer-body">
         <div class="container py-5">
           <div class="row py-4">
-            <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
-              {/*<p class="font-italic text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>*/}
-              <ul class="list-inline mt-4">
-                <li class="list-inline-item"><a href="#" target="_blank" title="twitter" ><FontAwesomeIcon icon={faTwitter} size="3x" color="#55ACEE" /></a></li>
-                <li class="list-inline-item"><a href="#" target="_blank" title="facebook"><FontAwesomeIcon icon={faFacebook} size="3x" color="#3B5998" /></a></li>
-                <li class="list-inline-item"><a href="#" target="_blank" title="instagram"><FontAwesomeIcon icon={faInstagram} size="3x" color="#cd486b" /></a></li>
-                <li class="list-inline-item"><a href="#" target="_blank" title="twitter" ><FontAwesomeIcon icon={faYoutube} size="3x" color="#FF0000" /></a></li>
-              </ul>
-            </div>
             <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
-              {/*<h6 class="text-uppercase font-weight-bold mb-4">Product</h6>*/}
+              <p className="company-name"> Excel Career</p><br />
+              <p className="company-description">We are excellence in providing educational career guidance for students in sriLanka.</p>
+            </div>
+
+            <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
               <ul class="list-unstyled mb-0 ul-list">
-                {/*<li class="mb-2"><a href="#" class="text-muted">How it works</a></li>
-                  <li class="mb-2"><a href="#" class="text-muted">Benefits</a></li>*/}
+                <li class="mb-2"><a href="#" class="text-muted">Home</a></li>
+                <li class="mb-2"><a href="#" class="text-muted">Programs</a></li>
                 <li class="mb-2"><a href="#" class="text-muted">Features</a></li>
               </ul>
             </div>
-            <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
-              {/*<h6 class="text-uppercase font-weight-bold mb-4">Support</h6>*/}
-              <ul class="list-unstyled mb-0 ul-list">
-                {/*<li class="mb-2"><a href="#" class="text-muted">Blog</a></li>*/}
-                <li class="mb-2"><a href="#" class="text-muted">Help</a></li>
-                <li class="mb-2"><a href="#" class="text-muted">FAQ</a></li>
-              </ul>
-            </div>
-            <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
-              {/*<h6 class="text-uppercase font-weight-bold mb-4">Connect With Us....</h6>*/}
-              <ul class="list-unstyled mb-0 ul-list">
-                {/*<li class="borderbox"><input type="text" placeholder="Enter your name" class="form-control border-0 shadow-0" /> </li> <br/>
-              <li><input type="email" placeholder="Enter your email address"  class="form-control border-0 shadow-0" /></li> 
-              <div class="input-group-append">
-                <button type="submit" class="btnsubmit"><FontAwesomeIcon icon={faPaperPlane} size="" color="#191970"/></button>
-    </div>*/}
+            <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
 
+              <ul class="list-unstyled mb-0 ul-list">
+
+                <div className="footer-contact">Contact Us</div>
                 <li class="mb-2"><a href="#" class="text-muted">info@gmail.com</a></li>
                 <li class="mb-2"><a href="#" class="text-muted">1234567890</a></li>
+              </ul>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
+              <div className="footer-icons">Follow us</div>
+              <ul class="list-inline mt-4">
+              <i class="fa fa-facebook"></i>
+                {/* <li class="list-inline-item"><a href="#" target="_blank" title="twitter" ><FontAwesomeIcon icon={faTwitter} size="2x" color="#55ACEE" /></a></li>
+                <li class="list-inline-item"><a href="#" target="_blank" title="facebook"><FontAwesomeIcon icon={faFacebook} size="2x" color="#3B5998" /></a></li>
+                <li class="list-inline-item"><a href="#" target="_blank" title="instagram"><FontAwesomeIcon icon={faInstagram} size="2x" color="#cd486b" /></a></li>
+                <li class="list-inline-item"><a href="#" target="_blank" title="twitter" ><FontAwesomeIcon icon={faYoutube} size="2x" color="#FF0000" /></a></li> */}
               </ul>
             </div>
 
@@ -58,7 +51,7 @@ const Footer = () => {
 
 
 
-      </footer>
+      </div>
 
 
     </div>
--- src/components/pages/Features.jsx
+++ src/components/pages/Features.jsx
@@ -3,26 +3,25 @@ import '../pages/features.css';
 import { Col, Row, Container } from "react-bootstrap";
 import { Link } from 'react-router-dom';
 import Carousel from 'react-bootstrap/Carousel';
-import imageone from "../images/mentor (1).jpg";
-import imagetwo from "../images/roadmap (1).jpg";
-import imagethree from "../images/webinar (1).jpg";
+import imageone from "../images/mentor.jpg";
+import imagetwo from "../images/roadmap.jpg";
+import imagethree from "../images/webinar.jpg";
 import image1 from "../images/quote1.jpg";
 import image2 from "../images/quote2.jpg";
 import image3 from "../images/quote3.jpg";
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
-//import "node_modules/video-react/dist/video-react.css";
-// import { Player } from 'video-react';
 import video from "../images/video.mp4";
-//import VideoPlayer from 'react-video-js-player';
+
 
 
 function Features() {
   return (
     <>
-      <div className="header-features">Explore more Features</div>
+    <div className="features">
+      <div className="header-features">Explore more Features</div><br />
       <div className="video">
-        <video width="600" height="500" align-items="center" controls >
+        <video width="900" height="500" align-items="center" controls >
           <source src={video} type="video/mp4" />
         </video>
       </div>
@@ -64,7 +63,7 @@ function Features() {
             <div class="features-card">
               <div class="card_image"><img src={imagethree}  alt="webinar " /></div>
               <div class="card_content">
-                <div class="card_title">Knowledge Sharing webinars <span class='right'></span></div>
+                <div class="card_title">Knowledge Sharing webinars </div>
                 <div class="card_text">
                   
                   <p className="features-p">Webinars will be available time to time to provide support to students to get proper knowledge in relavant educational area. Information related to webinars will be availabe in our webiste. </p>
@@ -74,6 +73,7 @@ function Features() {
             </div>
           </li>
         </ul>
+      </div>
       </div>
 </>
 
